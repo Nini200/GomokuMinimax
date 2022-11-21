d8115ab6be3184ff0ab0f9bfceef11cd364bff54
Weronika Raducka <weronikaraducka@interia.pl>
2022-11-21 17:46:06 +0100

    + Advanced shapes of consecutive stones - adding stones functionality

--- src/main/Board/ConsecutiveAdvStones.java
+++ src/main/Board/ConsecutiveAdvStones.java
@@ -32,6 +32,21 @@ public class ConsecutiveAdvStones extends ConsecutiveStones{
 
 
     public void addAdvancedConsecutiveStones(int stones, int openEnds, AdvancedStoneShapes shape) {
-
+        switch (shape) {
+            case NotAdvanced -> addConsecutiveStones(stones, openEnds);
+            case FourHoleAfter3 -> this.fourHoleAfter3++;
+            case FourHoleAfter2 -> this.fourHoleAfter2++;
+            case ThreeHoleAfter2Open2 -> this.threeHoleAfter2Open2++;
+            case ThreeHoleAfter2Blocked1 -> this.threeHoleAfter2Blocked1++;
+            case ThreeHoleAfter2Blocked2 -> this.threeHoleAfter2Blocked2++;
+            case ThreeHoles2InRow ->  this.threeHoles2InRow++;
+            case ThreeHoles2Separated -> this.threeHoles2Separated++;
+            case Three2OpenMax5 -> this.three2OpenMax5++;
+            case TwoOn5 -> this.twoOn5++;
+            case TwoOn4Open2 -> this.twoOn4Open2++;
+            case TwoOn4Open1 -> this.twoOn4Open1++;
+            case TwoOn3Open2 -> this. twoOn3Open2++;
+            case TwoOn3Open1 -> this. twoOn3Open1++;
+        }
     }
 }
